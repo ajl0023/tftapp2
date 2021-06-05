0340a9edb54987ba6a719dabc951f9e2a16eabe7
austin <austinlee199723@yahoo.com>
2021-06-04 20:59:36 -0700

    deleted build folder

--- api/index.js
+++ api/index.js
@@ -15,3 +15,20 @@
 //     require("./routes")(app);
 //   })
 //   .catch((err) => {});
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => res.send("Home Page Route"));
+
+app.get("/about", (req, res) => res.send("About Page Route"));
+
+app.get("/portfolio", (req, res) => res.send("Portfolio Page Route"));
+
+app.get("/contact", (req, res) => res.send("Contact Page Route"));
+
+const port = process.env.PORT || 7000;
+
+app.listen(port, () =>
+  console.log(`Server running on ${port}, http://localhost:${port}`)
+);
