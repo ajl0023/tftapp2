c45ec158f48f31f1313316fde179132f1590688f
austin <austinlee199723@yahoo.com>
2021-06-04 20:49:14 -0700

    added api folder for vercel

--- functions copy/index.js
+++ functions copy/index.js
@@ -1,17 +1,34 @@
-const express = require("express");
-const app = express();
-const axios = require("axios");
-const connect = require("./mongoUtil");
-var cors = require("cors");
-const path = require("path");
+// const express = require("express");
+// const app = express();
+// const axios = require("axios");
+// const connect = require("./mongoUtil");
+// var cors = require("cors");
+// const path = require("path");
 
-app.use(cors());
-app.use(express.json({ limit: "50mb" }));
-app.use(express.static(path.join(__dirname, "../tftapp/build")));
-connect
-  .connect()
-  .then(() => {
-    app.listen(7001, () => {});
-    require("./routes")(app);
-  })
-  .catch((err) => {});
+// app.use(cors());
+// app.use(express.json({ limit: "50mb" }));
+// app.use(express.static(path.join(__dirname, "../tftapp/build")));
+// connect
+//   .connect()
+//   .then(() => {
+//     app.listen(7001, () => {});
+//     require("./routes")(app);
+//   })
+//   .catch((err) => {});
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => res.send("Home Page Route"));
+
+app.get("/about", (req, res) => res.send("About Page Route"));
+
+app.get("/portfolio", (req, res) => res.send("Portfolio Page Route"));
+
+app.get("/contact", (req, res) => res.send("Contact Page Route"));
+
+const port = process.env.PORT || 7000;
+
+app.listen(port, () =>
+  console.log(`Server running on ${port}, http://localhost:${port}`)
+);
